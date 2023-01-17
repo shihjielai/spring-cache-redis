56961afba218c3c3ccfa326ccb984449c9b7c4c4
LAPTOP-2KQN7ANF\Jay <jay.lai@rawstone.com.tw>
2023-01-17 16:53:35 +0800

    Lua腳本 => 保證加鎖(佔位 + 過期時間)和解鎖(判斷 + 刪除)都是原子性

--- src/main/java/com/example/springbootrestapi/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootrestapi/service/impl/UserServiceImpl.java
@@ -15,10 +15,14 @@ import lombok.AllArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
@@ -90,12 +94,13 @@ public class UserServiceImpl implements UserService {
         }
 
         System.out.println("有快取，直接返回");
-        List<UserDto> userDtos = mapper.readValue(allUsers, new TypeReference<List<UserDto>>() {});
+        List<UserDto> userDtos = mapper.readValue(allUsers, new TypeReference<List<UserDto>>() {
+        });
 
         return userDtos;
     }
 
-    public List<UserDto> getAllUsersFromDB() throws JsonProcessingException {
+    public List<UserDto> getAllUsersFromDBWithLocalLock() throws JsonProcessingException {
 
         // 只要是同一把鎖，就能鎖住需要這個鎖的所有執行續
         // 1. synchronized (this): SpringBoot所有的component都是single instance
@@ -103,16 +108,68 @@ public class UserServiceImpl implements UserService {
         synchronized (this) {
 
             // 得到鎖以後，應該再去快取中確認是否存在，如果沒有才需要繼續查詢
-            String allUsers = stringRedisTemplate.opsForValue().get("allUsers");
-            if (StringUtils.isNotBlank(allUsers)) {
-                // 快取不為 null 直接回傳
-                List<UserDto> userDtos = mapper.readValue(allUsers, new TypeReference<List<UserDto>>() {});
+            return getUsersDtosFromDB();
+        }
 
-                return userDtos;
+    }
+
+    public List<UserDto> getAllUsersFromDBWithRedisLock() throws JsonProcessingException {
+
+        // 佔分布式鎖(去redis)
+        String uuid = UUID.randomUUID().toString();
+        Boolean lock = stringRedisTemplate.opsForValue().setIfAbsent("lock", uuid, 300, TimeUnit.SECONDS);
+
+        if (lock) {
+            // 加鎖成功 設定任務
+            // 設定過期時間必須和加鎖是同步的，原子的
+            //stringRedisTemplate.expire("lock", 30, TimeUnit.SECONDS);
+
+            List<UserDto> usersDtosFromDB;
+
+            try {
+                usersDtosFromDB = getUsersDtosFromDB();
+            } finally {
+                // 刪除鎖
+                String script = "if redis.call(\"get\",KEYS[1]) == ARGV[1]\n" +
+                        "then\n" +
+                        "    return redis.call(\"del\",KEYS[1])\n" +
+                        "else\n" +
+                        "    return 0\n" +
+                        "end";
+
+                Long lock1 = stringRedisTemplate.execute(new DefaultRedisScript<Long>(script, Long.class),
+                        Arrays.asList("lock"),
+                        uuid);
             }
 
-            System.out.println("查詢資料庫...");
-            List<User> users = userRepository.findAll();
+            //stringRedisTemplate.delete("lock"); //  刪除鎖
+
+            // 拿到值比對 + 比對成功刪除 = 原子操作 lua腳本解鎖
+
+//            String lockValue = stringRedisTemplate.opsForValue().get("lock");
+//            if (uuid.equals(lockValue)) {
+//                // 刪除自己的鎖
+//                stringRedisTemplate.delete("lock");
+//            }
+            return usersDtosFromDB;
+        } else {
+            // 加鎖失敗...重試
+            return getAllUsersFromDBWithRedisLock();
+        }
+    }
+
+    private List<UserDto> getUsersDtosFromDB() throws JsonProcessingException {
+        String allUsers = stringRedisTemplate.opsForValue().get("allUsers");
+        if (StringUtils.isNotBlank(allUsers)) {
+            // 快取不為 null 直接回傳
+            List<UserDto> userDtos = mapper.readValue(allUsers, new TypeReference<List<UserDto>>() {
+            });
+
+            return userDtos;
+        }
+
+        System.out.println("查詢資料庫...");
+        List<User> users = userRepository.findAll();
 //        List<UserDto> userDtos = users.stream()
 //                .map(user -> userMapper.mapToUserDto(user))
 //                .collect(Collectors.toList());
@@ -120,16 +177,14 @@ public class UserServiceImpl implements UserService {
 //                        .map(user -> modelMapper.map(user, UserDto.class))
 //                        .collect(Collectors.toList());
 
-            List<UserDto> userDtos = users.stream()
-                    .map(user -> AutoUserMapper.autoUserMapper.mapToUserDto(user))
-                    .collect(Collectors.toList());
+        List<UserDto> userDtos = users.stream()
+                .map(user -> AutoUserMapper.autoUserMapper.mapToUserDto(user))
+                .collect(Collectors.toList());
 
-            // 查到的資料放入快取，將其轉為json放入快取
-            String jsonString = mapper.writeValueAsString(userDtos);
-            stringRedisTemplate.opsForValue().set("allUsers", jsonString);
-            return userDtos;
-        }
-
+        // 查到的資料放入快取，將其轉為json放入快取
+        String jsonString = mapper.writeValueAsString(userDtos);
+        stringRedisTemplate.opsForValue().set("allUsers", jsonString);
+        return userDtos;
     }
 
     @Override
